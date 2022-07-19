2da76a26ca78457ea3f3766a53da8e8dcf66b48f
AlderAutomation <mheltman@alderautomation.ca>
2022-07-19 12:51:52 -0700

    re-add card class

---
+++ src/card.py
@@ -0,0 +1,22 @@
+class card:
+    
+    def __init__(self, number, suit, face="down", rotation="portrait" ) -> None:
+        self.number = number
+        self.suit = suit
+        self.face = face
+        self.rotation = rotation
+
+        if suit == "hearts":
+            self.colour = "red"
+        elif suit == "diamonds":
+            self.colour = "red"
+        elif suit == "clubs":
+            self.colour = "black"
+        elif suit == "spades":
+            self.colour = "black"
+
+    def flip_card (self) -> None:
+        if self.face == "up":
+            self.face = "down"
+        else:
+            self.face = "up"
