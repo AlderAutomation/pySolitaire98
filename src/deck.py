39027a9a95b429351e0e5c53abea21333854fa1d
AlderAutomation <mheltman@alderautomation.ca>
2022-10-15 17:48:38 -0700

    add logging

--- src/deck.py
+++ src/deck.py
@@ -3,13 +3,16 @@ import card
 import spritesheet
 import pygame
 import settings
+import my_logger
 
+my_log = my_logger.Default().my_logger
 
 numbers = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
 suits = ['clubs', 'diamonds', 'hearts', 'spades']
 
 class Deck:
     def __init__(self) -> None:
+        my_log.debug('Building Deck')
         self.is_out_of_cards = False
         image = pygame.image.load("./assets/image/spritesheet.png").convert_alpha()
         sprite_sheet =  spritesheet.SpriteSheet(image)
@@ -23,6 +26,7 @@ class Deck:
 
 
     def shuffle(self) -> None:
+        my_log.debug('Shuffling deck')
         random.shuffle(self.card_deck)
 
     
@@ -30,6 +34,7 @@ class Deck:
         if len(self.card_deck) == 0:
             return False
         else:
+            my_log.debug(f'Dealing {self.card_deck[index]}')
             return self.card_deck.pop(index)
 
     
@@ -39,10 +44,12 @@ class Deck:
 
 
     def reset_deck(self):
+        my_log.debug('Resetting Deck')
         self.__init__()
 
 
     def rebuild_from_discard(self, card:object) -> None:
+        my_log.debug('Rebuilding deck from waste pile')
         card.face = "down"
         card.is_covered = True
         self.card_deck.append(card)
